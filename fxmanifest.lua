20ab1c56fdb2e2657f110d0f5b8324b449c349f2
Kylend <95599217+dnelyk@users.noreply.github.com>
2022-09-04 20:42:17 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'cdn-goldheist'
+version '1.0'
+author 'dnelyK'
+
+server_script 'server/*.lua' -- Execute everything that is a .lua in the server folder of the resource
+client_script 'client/*.lua' -- Execute everything that is a .lua in the client folder of the resource
+shared_script 'shared/config.lua' -- Share everything the is in the specified file throughout everything in the resource.
